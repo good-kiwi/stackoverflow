88f573ec19ac42401425a64ed0fe8110237d06d7
Joseph <joseph.cal@gmail.com>
2020-06-15 10:26:42 -0500

    data exploration and model building

--- AirBNB_Boston.py
+++ AirBNB_Boston.py
@@ -5,9 +5,12 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
 import seaborn as sns
+from scipy import stats
+import math
+
 
 def clean_data(df):
-    '''
+    """
     INPUT
     df - pandas dataframe
 
@@ -16,33 +19,93 @@ def clean_data(df):
     y - the corresponding response vector
 
     This function cleans df using the following steps to produce X and y:
-    1. Drop all the rows with no salaries
-    2. Create X as all the columns that are not the Salary column
-    3. Create y as the Salary column
-    4. Drop the Salary, Respondent, and the ExpectedSalary columns
-    5. For each numeric variable, fill the column with the mean value.
-    6. Create dummy columns for all the categorical variables, drop the original columns
-    '''
-    # Drop rows with missing salary values
-    df = df.dropna(subset=['Salary'], axis=0)
-    y = df['Salary']
+    1. Drop rows with 0 price.
+    2. Create y as the price column, transformed by log
+    3. Create X from selected columns
+    4. Deal with missing values
+    5. Create dummy variables for selected categorical variables, drop the original columns
+    """
 
-    #Drop respondent and expected salary columns
-    df = df.drop(['Respondent', 'ExpectedSalary', 'Salary'], axis=1)
+    # Drop rows with 0 price
+    df = df[df.price > 0]
 
-    # Fill numeric columns with the mean
-    num_vars = df.select_dtypes(include=['float', 'int']).columns
-    for col in num_vars:
-        df[col].fillna((df[col].mean()), inplace=True)
+    # Create y
+    y = df['price'].apply(math.log)
 
+    # Select columns for X
+    selected_vars = ['host_listings_count',
+                     'calculated_host_listings_count_private_rooms',
+                     'neighbourhood_cleansed',
+                     'room_type',
+                     'beds',
+                     'availability_365',
+                     'number_of_reviews',
+                     'neighborhood_overview',
+                     'space',
+                     'notes',
+                     'transit',
+                     'access',
+                     'interaction',
+                     'house_rules',
+                     'host_about',
+                     'host_is_superhost',
+                     'host_has_profile_pic',
+                     'host_identity_verified',
+                     'instant_bookable',
+                     'require_guest_profile_picture',
+                     'require_guest_phone_verification',]
+
+    bool_vars = ['host_is_superhost',
+                 'host_has_profile_pic',
+                 'host_identity_verified',
+                 'instant_bookable',
+                 'require_guest_profile_picture',
+                 'require_guest_phone_verification']
+
+    free_text_vars = ['neighborhood_overview',
+                      'space',
+                      'notes',
+                      'transit',
+                      'access',
+                      'interaction',
+                      'house_rules',
+                      'host_about']
+
+    df = df[selected_vars]
+    # Deal with missing values
+    df['number_of_reviews'].fillna(0, inplace=True)
+    df[bool_vars].fillna('f', inplace=True)
+    df[free_text_vars].fillna('', inplace=True)
+
+    def translate_bool(col):
+        for index, value in col.iteritems():
+            col[index] = 1 if value == 't' else 0
+        return col
+
+    def create_bool(col):
+        for index, value in col.iteritems():
+            col[index] = 0 if value == '' else 1
+        return col
+
+    fill_mean = lambda col: col.fillna(col.mean())
+    num_vars = df.select_dtypes(include=['int', 'float']).columns
+    df[num_vars] = df[num_vars].apply(fill_mean, axis=0)
+    df[bool_vars] = df[bool_vars].apply(translate_bool, axis=0)
+    df[bool_vars].dtype = int
+    df[free_text_vars] = df[free_text_vars].apply(create_bool, axis=0)
+    df[free_text_vars].dtype = int
+    for col in bool_vars:
+        print(col, ":", df[col].apply(type).value_counts())
     # Dummy the categorical variables
-    cat_vars = df.select_dtypes(include=['object']).copy().columns
-    for var in  cat_vars:
+    cat_vars = ['neighbourhood_cleansed', 'room_type']
+    for var in cat_vars:
         # for each cat add dummy var, drop original column
         df = pd.concat([df.drop(var, axis=1), pd.get_dummies(df[var], prefix=var, prefix_sep='_', drop_first=True)], axis=1)
 
     X = df
+
     return X, y
+
 
 def find_optimal_lm_mod(X, y, cutoffs, test_size = .30, random_state=42, plot=True):
     '''
@@ -105,26 +168,217 @@ def find_optimal_lm_mod(X, y, cutoffs, test_size = .30, random_state=42, plot=Tr
 
     return r2_scores_test, r2_scores_train, lm_model, X_train, X_test, y_train, y_test
 
+
 def main():
-    #df = pd.read_csv('data/survey-results-public.csv')
+    plot = False  # set to true if you would like to see plots
+    print_log = False  # set to true if you would like to see stats outputted to console
+    print_result = True
 
-    #X, y = clean_data(df)
-    #cutoffs here pertains to the number of missing values allowed in the used columns.
-    #Therefore, lower values for the cutoff provides more predictors in the model.
-    #cutoffs = [5000, 3500, 2500, 1000, 100, 50, 30, 20, 10, 5]
-
-    #r2_scores_test, r2_scores_train, lm_model, X_train, X_test, y_train, y_test = find_optimal_lm_mod(X, y, cutoffs, plot=False)
-    #print('Finished Finding the Best Model')
-    #return lm_model
+    # Data Exploration
     desired_width=320
     pd.set_option('display.width', desired_width)
-    pd.set_option('display.max_columns', 12)
-    df_listings = pd.read_csv('data/listings_boston.csv')
-    print(df_listings.describe())
+    pd.set_option('display.max_columns', 50)
+
+    # Get a sense of the numerical data in the available datasets.
+    df_listings = pd.read_csv('data/listings_boston.csv', dtype={"price": str,
+                                                                 "weekly_price": str,
+                                                                 "monthly_price": str,
+                                                                 "security_deposit": str,
+                                                                 "cleaning_fee": str,
+                                                                 "extra_people": str,
+                                                                 "host_response_rate": str})
+    # clean up price data to make it numeric
+    df_listings.loc[:, "price"] = df_listings["price"].str.replace(',', '').str.replace('$', '').astype('float')
+    df_listings.loc[:, "weekly_price"] = df_listings["weekly_price"].str.replace(',', '').str.replace('$', '').astype('float')
+    df_listings.loc[:, "monthly_price"] = df_listings["monthly_price"].str.replace(',', '').str.replace('$', '').astype('float')
+    df_listings.loc[:, "security_deposit"] = df_listings["security_deposit"].str.replace(',', '').str.replace('$', '').astype('float')
+    df_listings.loc[:, "cleaning_fee"] = df_listings["cleaning_fee"].str.replace(',', '').str.replace('$', '').astype('float')
+    df_listings.loc[:, "extra_people"] = df_listings["extra_people"].str.replace(',', '').str.replace('$', '').astype('float')
+    df_listings["host_response_rate"].fillna("0", inplace=True)
+    df_listings.loc[:, "host_response_rate"] = df_listings["host_response_rate"].str.replace('%', '').astype('int')
+    if print_log:
+        print(df_listings.describe())
     df_neighborhoods = pd.read_csv('data/neighbourhoods_boston.csv')
-    print(df_neighborhoods.describe())
+    if print_log:
+        print(df_neighborhoods.describe())
     df_reviews = pd.read_csv('data/reviews_boston.csv')
-    print(df_reviews.describe())
+    if print_log:
+        print(df_reviews.describe())
+    df_calendar = pd.read_csv('data/calendar_boston.csv', dtype={"price": str, "adjusted_price": str})
+    # clean up price data to make it numeric
+    df_calendar.loc[:, "price"] = df_calendar["price"].str.replace(',', '').str.replace('$', '').astype('float')
+    df_calendar.loc[:, "adjusted_price"] = df_calendar["adjusted_price"].str.replace(',', '').str.replace('$', '').astype('float')
+    if print_log:
+        print(df_calendar.describe())
+
+    # df_neighborhoods is basically empty and can be ignored
+    # df_reviews is full of unstructured review data and would have to be mined to produce modelable data
+    # df_listings has descriptive information about the location
+    # df_calendar has price information and how it varies over time. Price and adjusted price have to be formatted.
+    # Going to primarily focus on df_listings
+
+    # How many N/A values are there for each column?
+    if print_log:
+        for col in df_listings.columns:
+            print(col, ':', df_listings[col].dropna().shape[0] / df_listings[col].shape[0])
+
+    # Possible binary variable conversions: neighborhood_overview, space, notes, transit, access, interaction,
+    # house_rules
+
+    # Are there any correlations we should worry about?
+    num_vars = ["price",
+                "weekly_price",
+                "monthly_price",
+                "security_deposit",
+                "cleaning_fee",
+                "extra_people",
+                'host_listings_count',
+                'host_total_listings_count',
+                'calculated_host_listings_count',
+                'calculated_host_listings_count_entire_homes',
+                'calculated_host_listings_count_private_rooms',
+                'calculated_host_listings_count_shared_rooms',
+                'host_response_rate',
+                'accommodates',
+                'bathrooms',
+                'bedrooms',
+                'beds',
+                'square_feet',
+                'guests_included',
+                'minimum_nights',
+                'minimum_minimum_nights',
+                'maximum_minimum_nights',
+                'minimum_nights_avg_ntm',
+                'maximum_nights',
+                'minimum_maximum_nights',
+                'maximum_maximum_nights',
+                'maximum_nights_avg_ntm',
+                'availability_30',
+                'availability_60',
+                'availability_90',
+                'availability_365',
+                'number_of_reviews',
+                'number_of_reviews_ltm',
+                'reviews_per_month',
+                'review_scores_rating',
+                'review_scores_accuracy',
+                'review_scores_cleanliness',
+                'review_scores_checkin',
+                'review_scores_communication',
+                'review_scores_location',
+                'review_scores_value'
+                ]
+    if plot:
+        sns.heatmap(df_listings[num_vars].corr(), annot=False, fmt=".2f", cmap="YlGnBu", linewidths=.5, square=True)
+        plt.show()
+
+    # Correlation matrix supports some clearly distinct categories of data
+    # Pricing: price, weekly_price, monthly_price, security_deposit, cleaning_fee, extra_people
+    # Host: host_listings_count, host_total_listings_count, calculated_host_listings_count,
+    #  calculated_host_listings_count_entire_homes, calculated_host_listings_count_private_rooms,
+    #  calculated_host_listings_count_shared_rooms
+    # Property: accommodates, bathrooms, bedrooms, beds, square_feet, guests_included, minimum_nights,
+    #  minimum_minimum_nights, maximum_minimum_nights, minimum_nights_avg_ntm, maximum_nights, minimum_maximum_nights,
+    #  maximum_maximum_nights, maximum_nights_avg_ntm
+    # Availability: availability_30, availability_60, availability_90, availability_365
+    # Reviews: number_of_reviews, number_of_reviews_ltm, reviews_per_month, review_scores_rating,
+    #  review_scores_cleanliness, review_scores_checkin, review_scores_communication, review_scores_location,
+    #  review_scores_value
+
+    # Get a sense of the categorical data in the available data.
+    cat_vars = ["space",
+                "description",
+                "experiences_offered",
+                "neighborhood_overview",
+                "notes",
+                "transit",
+                "access",
+                "interaction",
+                "house_rules",
+                "host_name",
+                "host_since",
+                "host_location",
+                "host_about",
+                "host_response_time",
+                "host_acceptance_rate",
+                "host_is_superhost",
+                "host_neighbourhood",
+                "host_verifications",
+                "host_has_profile_pic",
+                "host_identity_verified",
+                "street",
+                "neighbourhood",
+                "neighbourhood_cleansed",
+                "market",
+                "smart_location",
+                "is_location_exact",
+                "property_type",
+                "room_type",
+                "bed_type",
+                "amenities",
+                "extra_people",
+                "calendar_updated",
+                "has_availability",
+                "calendar_last_scraped",
+                "requires_license",
+                "instant_bookable",
+                "is_business_travel_ready",
+                "cancellation_policy",
+                "require_guest_profile_picture",
+                "require_guest_phone_verification"]
+
+    if print_log:
+        for col in df_listings[cat_vars].columns:
+            print(df_listings[[col, 'price']].groupby([col]).mean())
+
+    # free text columns: space, description, neighborhood_overview, notes, transit, access, interaction, house_rules,
+    #  host_name, host_about,
+    # empty: experiences_offered, market, calendar_last_scraped, requires_license, is_business_travel_ready
+    # boolean: host_is_superhost, host_has_profile_pic, host_identity_verified, is_location_exact, has_availability,
+    #  instant_bookable, require_guest_profile_picture, require_guest_phone_verification, host_about
+    # categorical: property_type, room_type, bed_type, amenities, calendar_updated, cancellation_policy,
+
+    if print_log:
+        print(pd.crosstab(df_listings['neighbourhood'], df_listings['room_type']))
+    # Surprised to see that the top neighborhoods are not very desirable areas to vacation in.
+    # Also the majority of the listings are for an entire unit.
+
+    # Explore target variable, price
+    target = df_listings['price'].copy()
+    if plot:
+        plt.hist(target, bins=[0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000])
+        plt.show()
+    # how many zero values are there?
+    if print_log:
+        print("Number of 0 price listings: ", (target == 0).sum())
+
+    # Need to drop 0 price listings
+    target = target[target != 0]
+
+    # Drop outlier prices
+    # target = target[target <= 4000]
+
+    # seems like a log-normal distribution
+    shape, loc, scale = stats.lognorm.fit(target)
+    if print_log:
+        print(shape, loc, scale)
+        print(stats.kstest(target, "lognorm", args=[shape, loc, scale]))
+    if plot:
+        sns.distplot(target, fit=stats.lognorm, kde=False, rug=True)
+        plt.show()
+        linspace = np.linspace(0, 1000, 100)
+        pdf_lognorm = stats.lognorm.pdf(linspace, shape, loc, scale)
+        plt.hist(target, density=True, bins=[0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000])
+        plt.plot(linspace, pdf_lognorm, label="lognorm")
+        plt.show()
+    X, y = clean_data(df_listings)
+    r2_scores_test, r2_scores_train, lm_model, X_train, X_test, y_train, y_test = find_optimal_lm_mod(X, y, [100, 1000, 2000, 3000])
+    if print_result:
+        print('test r2: ', r2_scores_test)
+        print('train r2:', r2_scores_train)
+        print(list(zip(X_train.columns, np.exp(lm_model.coef_))))
+
+    return lm_model
 
 
 if __name__ == '__main__':
